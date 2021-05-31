15c4e2ab19e1b1a77781acd1fb2573c6a5d85286
3atmancoding <3atmancoding@gmail.com>
2021-05-31 21:12:54 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let days = { Yesterday: 'yesterday', 'Two Days Ago': 'twoDaysAgo' };
+  const getInputValue = useRef();
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+  const [error, renderError] = useState('');
+  const [data, setData] = useState([]);
+  const [param, setParam] = useState('yesterday');
+  const getCaseDetails = useCallback(async () => {
+    try {
+      let response = await axios.get(
+        `https://disease.sh/v3/covid-19/continents?${param}=yesterday&sort=active`
+      );
+      console.log('The response', response);
+      setData(response.data);
+    } catch (err) {
+      setError(true);
+      console.log(err.message);
+      renderError(err.message);
+    }
+  }, [param]);
+  ///////////////////////////////
+  const onChange = (e) => {
+    e.preventDefault();
+    console.log('Submitted');
+    const refValue = getInputValue.current.value;
+    setParam(refValue);
+  };
+  //////////////////////////////////
+
+  const dayMap = Object.keys(days).map((key) => {
+    return (
+      <option value={days[key]} key={uuidv4()}>
+        {key}
+      </option>
+    );
+  });
+
+  //////////////////////////////////
+  useEffect(() => {
+    getCaseDetails();
+    setLoading(false);
+  }, [getCaseDetails]);
+
+  if (Boolean(isError)) {
+    return <>Oops! That's an error : {error}</>;
+  } else if (Boolean(isLoading)) {
+    return <>Loading...</>;
+  } else {
+    return (
+      <>
+        {data.map((item) => (
+          <li key={uuidv4()}>
+            ActiveCases: {item.active} in {item.continent}
+          </li>
+        ))}
+        <select onChange={onChange} ref={getInputValue}>
+          {dayMap}
+        </select>
+      </>
+    );
+  }
 }
 
 export default App;
